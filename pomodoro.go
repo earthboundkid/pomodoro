44a3e79714259e6da3ff8e5db03fd082afe9c6a1
Max Lorenz <max-lorenz@gmx.net>
2016-02-15 09:30:29 +0100

    Simplified flags, reduced time options

--- pomodoro.go
+++ pomodoro.go
@@ -1,50 +1,46 @@
-// pormodoro timer
 package main
 
 import (
 	"errors"
 	"flag"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/0xAX/notificator"
 )
 
-const defaultDuration = 25 * time.Minute
+var duration string
+var silence bool
 
-var silence = flag.Bool("silence", false, "")
+var notify *notificator.Notificator
 
 func init() {
-	const usage = `Usage of pomodoro:
-
-	pomodoro [-silence] [duration]
-
-Duration defaults to %d minutes. Durations may be expressed as integer minutes
-(e.g. "15") or time with units (e.g. "1m30s" or "90s").
-
-Chimes system bell at the end of the timer, unless -silence is set.
-`
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, usage, int(defaultDuration/time.Minute))
-	}
+  const (
+    defaultDuration = "25m"
+    durationUsage = "Duration, e.g. 1m30s or 90s"
+    silenceUsage = "Suppresses the notification after the timer ends"
+  )
+  
+  flag.StringVar(&duration, "duration", defaultDuration, durationUsage)
+  flag.StringVar(&duration, "d", defaultDuration, durationUsage+" (shorthand)")
+  flag.BoolVar(&silence, "silence", false, silenceUsage)
+  flag.BoolVar(&silence, "s", false, silenceUsage+" (shorthand)")
 	flag.Parse()
+  
+  notify = notificator.New(notificator.Options{
+	 DefaultIcon: "icon/default.png",
+	 AppName:     "Pomodoro",
+  })
 }
 
 func waitDuration() (time.Duration, error) {
-	if len(flag.Args()) > 1 {
-		return 0, errors.New("Too many args...")
-	}
-
-	if flag.Arg(0) == "" {
-		return defaultDuration, nil
-	}
-
-	if n, err := strconv.Atoi(flag.Arg(0)); err == nil {
+	if n, err := strconv.Atoi(duration); err == nil {
 		return time.Duration(n) * time.Minute, nil
 	}
 
-	if d, err := time.ParseDuration(flag.Arg(0)); err == nil {
+	if d, err := time.ParseDuration(duration); err == nil {
 		return d, nil
 	}
 
@@ -63,31 +59,10 @@ func countdown(target time.Time, formatDuration func(time.Duration) string) {
 	}
 }
 
-func formatDays(timeLeft time.Duration) string {
-	days := int(timeLeft.Hours() / 24)
-	hours := int(timeLeft.Hours()) % 24
-	minutes := int(timeLeft.Minutes()) % 60
-	seconds := int(timeLeft.Seconds()) % 60
-	return fmt.Sprintf("Countdown: %d:%02d:%02d:%02d \r",
-		days, hours, minutes, seconds)
-}
-
-func formatHours(timeLeft time.Duration) string {
-	hours := int(timeLeft.Hours())
-	minutes := int(timeLeft.Minutes()) % 60
-	seconds := int(timeLeft.Seconds()) % 60
-	return fmt.Sprintf("Countdown: %d:%02d:%02d \r",
-		hours, minutes, seconds)
-}
-
 func formatMinutes(timeLeft time.Duration) string {
 	minutes := int(timeLeft.Minutes())
 	seconds := int(timeLeft.Seconds()) % 60
 	return fmt.Sprintf("Countdown: %d:%02d \r", minutes, seconds)
-}
-
-func formatSeconds(timeLeft time.Duration) string {
-	return fmt.Sprintf("Countdown: %02.1f \r", math.Abs(timeLeft.Seconds()))
 }
 
 func main() {
@@ -99,21 +74,9 @@ func main() {
 	doneT := time.Now().Add(wait)
 
 	fmt.Printf("Start timer for %s.\n\n", wait)
+	countdown(doneT, formatMinutes)
 
-	switch {
-	case wait >= 24*time.Hour:
-		countdown(doneT, formatDays)
-	case wait >= time.Hour:
-		countdown(doneT, formatHours)
-	case wait >= time.Minute:
-		countdown(doneT, formatMinutes)
-	default:
-		countdown(doneT, formatSeconds)
-	}
-
-	if !*silence {
-		fmt.Println("\a") // \a is the bell literal.
-	} else {
-		fmt.Println()
+	if !silence {
+		notify.Push("Done!", "You finished your pomodoro", "", notificator.UR_NORMAL)
 	}
 }
